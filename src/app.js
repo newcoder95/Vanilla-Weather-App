f544010992a3f274bf4dc57853abb26a2e76e369
Alina <alina.pechenkina20@gmail.com>
2022-06-07 12:18:59 +0300

    js current weather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response){
+   let temperatureElement = document.querySelector("#temperature");
+   let cityElement = document.querySelector("#city")
+   let description = document.querySelector("#description");
+   let humidityElement = document.querySelector("#humidity");
+   let windElement = document.querySelector("#wind");
+   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+   cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+   description.innerHTML = response.data.weather[0].description;
+   humidityElement.innerHTML = response.data.main.humidity;
+   windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+
+let apiKey = "d408beb6fdb204fdf27972516e99f835";
+let apiUrl =
+  `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
 
