a8b844d0916b058363afd4b2e8cd3fe1ee0bde15
Alina <alina.pechenkina20@gmail.com>
2022-06-08 16:06:42 +0300

    implemented the units conversion

--- src/app.js
+++ src/app.js
@@ -1,60 +1,88 @@
 function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+   let date = new Date(timestamp);
+   let hours = date.getHours();
+   if (hours < 10) {
+      hours = `0${hours}`;
+   }
+   let minutes = date.getMinutes();
+   if (minutes < 10) {
+      minutes = `0${minutes}`;
+   }
+   let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+   ];
+   let day = days[date.getDay()];
+   return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#current-date");
-  let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
-  description.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+   let temperatureElement = document.querySelector("#temperature");
+   let cityElement = document.querySelector("#city");
+   let description = document.querySelector("#description");
+   let humidityElement = document.querySelector("#humidity");
+   let windElement = document.querySelector("#wind");
+   let dateElement = document.querySelector("#current-date");
+   let iconElement = document.querySelector("#icon");
+
+   celsiusElement = response.data.main.temp;
+   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+   cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+   description.innerHTML = response.data.weather[0].description;
+   humidityElement.innerHTML = response.data.main.humidity;
+   windElement.innerHTML = Math.round(response.data.wind.speed);
+   iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+   );
+   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(city){
+function search(city) {
    let apiKey = "d408beb6fdb204fdf27972516e99f835";
    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
    axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+   event.preventDefault();
+   let cityInputElement = document.querySelector("#city-input");
+   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemp(event) {
+   event.preventDefault();
+   let temperatureElement = document.querySelector("#temperature");
+   //remove active link to celsius
+   celsiusLink.classList.remove("active");
+   fahrenheitLink.classList.add("active");
+   let fahrenheitTemp = (celsiusElement * 9) / 5 + 32;
+   temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
 
+function displayCelsiusTemp(event) {
+   event.preventDefault();
+   let temperatureElement = document.querySelector("#temperature");
+   temperatureElement.innerHTML = Math.round(celsiusElement);
+   celsiusLink.classList.add("active");
+   fahrenheitLink.classList.remove("active");
+}
+
+let celsiusElement = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+search("Bratislava");
