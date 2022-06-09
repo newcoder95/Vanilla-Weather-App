9edf13f8f10446121dbc87bea064ae73f840d551
Alina <alina.pechenkina20@gmail.com>
2022-06-09 09:19:23 +0300

    updated functions

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function displayTemperature(response) {
    let description = document.querySelector("#description");
    let humidityElement = document.querySelector("#humidity");
    let windElement = document.querySelector("#wind");
+   let pressureElement = document.querySelector("#pressure");
    let dateElement = document.querySelector("#current-date");
    let iconElement = document.querySelector("#icon");
 
@@ -37,12 +38,14 @@ function displayTemperature(response) {
    description.innerHTML = response.data.weather[0].description;
    humidityElement.innerHTML = response.data.main.humidity;
    windElement.innerHTML = Math.round(response.data.wind.speed);
+   
    dateElement.innerHTML = formatDate(response.data.dt * 1000);
    iconElement.setAttribute(
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
    );
    iconElement.setAttribute("alt", response.data.weather[0].description);
+   pressureElement.innerHTML = Math.round(response.data.main.pressure * 0.750062);
 }
 
 function search(city) {
@@ -56,6 +59,23 @@ function handleSubmit(event) {
    let cityInputElement = document.querySelector("#city-input");
    search(cityInputElement.value);
 }
+
+function searchLocation(position) {
+   let apiKey = "d408beb6fdb204fdf27972516e99f835";
+   let latitude = position.coords.latitude;
+   let longitude = position.coords.longitude;
+   let units = "metric";
+   let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
+   let apiUrl = `${apiEndPoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+   axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+   event.preventDefault();
+   navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+
 
 function displayFahrenheitTemp(event) {
    event.preventDefault();
@@ -86,4 +106,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
-search("Bratislava");
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search("Сушки");
