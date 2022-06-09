f46f59931c813eb22c753993853ff2c3f61e76ec
Alina <alina.pechenkina20@gmail.com>
2022-06-09 09:22:50 +0300

    финальные правки

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function displayTemperature(response) {
    description.innerHTML = response.data.weather[0].description;
    humidityElement.innerHTML = response.data.main.humidity;
    windElement.innerHTML = Math.round(response.data.wind.speed);
-   
+
    dateElement.innerHTML = formatDate(response.data.dt * 1000);
    iconElement.setAttribute(
       "src",
