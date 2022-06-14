90298d5be1fdf16feb8771122bcb643d3acbbe1f
Alina <alina.pechenkina20@gmail.com>
2022-06-14 15:20:59 +0300

    add JS and forecast row

--- src/app.js
+++ src/app.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
    return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+forecastHTML = forecastHTML +
+  `
+    <div class="col-2">
+    ${day}
+   <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="forecast">
+   <div class="weather-forecast-temperatures">
+   <span class="weather-forecast-temperature-max">
+   18°
+   </span>
+   <span class="weather-forecast-temperature-min">
+   12°
+   </span>
+   </div>
+   </div>
+   `
+  });
+  
+  
+   forecastHTML = forecastHTML +`</div>`;
+   forecastElement.innerHTML = forecastHTML;
+};
+
+   
 function displayTemperature(response) {
    let temperatureElement = document.querySelector("#temperature");
    let cityElement = document.querySelector("#city");
@@ -109,4 +137,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemp);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-search("Сушки");
+search("Київ");
+displayForecast()
